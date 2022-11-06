7689d6a9c8e4a26d31d004dfc23468faec2d6b2a
DollarSys <116037907+DollarSys@users.noreply.github.com>
2022-11-06 10:31:12 +0200

    Update messageCreate.js

--- events/line/messageCreate.js
+++ events/line/messageCreate.js
@@ -7,7 +7,7 @@ module.exports = {
     if(message.content.startsWith('خط')) {
       if(!message.member.roles.cache.some((role) => role.id === config.seller_role)) return;
       await message.channel.send("\`-\` شيكو هنا ، لعبة عجلة الحظ بـ نظام طرب <#999294509223530597> 👌.")
-      await message.channel.send({ content: "https://cdn.discordapp.com/attachments/1034420702192865350/1034425987955441704/unknown.png" })
+      await message.channel.send({ content: "https://cdn.discordapp.com/attachments/1007634941368078337/1038732094593056798/unknown.png" })
       await message.delete().catch(async (err) => console.log("I Can't "));
     }
   }
