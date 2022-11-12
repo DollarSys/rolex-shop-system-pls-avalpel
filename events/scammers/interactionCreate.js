f87419a10344060c3b017664926af3d38b4ef17d
DollarSys <116037907+DollarSys@users.noreply.github.com>
2022-11-12 16:11:11 +0200

    Update interactionCreate.js

--- events/scammers/interactionCreate.js
+++ events/scammers/interactionCreate.js
@@ -20,10 +20,9 @@ module.exports = {
       if(!author) return inter.reply({ content: `❌ **I can't find the author of report.**`, ephemeral: true });
       let channel = inter.guild.channels.cache.get(config.scammer_channel);
       if(!channel) return inter.reply({ content: `❌ **I can't find scammer channel.**`, ephemeral: true });
-      inter.reply({ content: `✅ **تم قبول هذا البلاغ بنجاح.**`, ephemeral: true });
-      let embed = new Discord.EmbedBuilder()
-      .setTitle(`# Rolex Owner's`)
-      .setDescription(`\`===========================\`
+      inter.reply({ content: `✅ **تم قبول هذا البلاغ بنجاح.**`, ephemeral: true });      
+      await channel.send({ content: `<@&${config.scammer_role}> **بلاع علي نصاب.**
+\`===========================\`
 اسم النصاب: **${user.username}** - ايدي النصاب: ${user.id}
 \`===========================\`
 اسم المٌبلغ: **${author.username}** - ايدي المُبلغ: ${author.id}
@@ -32,10 +31,9 @@ module.exports = {
 \`===========================\`
 القصة: **${data.story}**
 \`===========================\`
-الدلائل:`)
-      await channel.send({ content: `<@&${config.scammer_role}> **بلاع علي نصاب.**`, embeds: [embed]})
-      // , files: `${data.photos.join("\n")}` 
-      await channel.send({ content: `وانت الإن يمكنك التبليغ علي اي شخص نصب عليك عن طريقة كتابة ، \`#بلاغ\` هنا <#1034108906076651550>` })
+الدلائل:
+${data.photos.join("\n")}`});
+      await channel.send(`وانت الإن يمكنك التبليغ علي اي شخص نصب عليك عن طريقة كتابة ، \`#بلاغ\` هنا <#1034108906076651550>`)
       data.status = true;
       await data.save().catch(() => 0);
       inter.message.edit({ content: `Accepted.`, components: [] }).catch(() => 0);
@@ -63,4 +61,4 @@ module.exports = {
       }
     }
   }
-}
+}*/
