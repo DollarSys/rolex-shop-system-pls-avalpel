7c5bf1dfb169aad794cd054e3327cb7d011473cc
DollarSys <116037907+DollarSys@users.noreply.github.com>
2022-11-13 12:34:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
 const client = new Client({ partials: [Partials.Message, Partials.Channel, Partials.User, Partials.GuildMember], intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages] });
-/*
-setInterval(async () => {
-  if(!client || !client.user) {
-    await process.kill(1);
-  }
-}, 60000);*/
+
 
 require("mongoose").connect(process.env.database, { useNewUrlParser: true, useUnifiedTopology: true });
 
