8a422d92d9bcb121c7e90bbd9c0c55a57fef9dd9
DollarSys <116037907+DollarSys@users.noreply.github.com>
2022-11-13 12:29:41 +0200

    Update order.js

--- commands/order/order.js
+++ commands/order/order.js
@@ -5,8 +5,8 @@ module.exports = {
   name: "طلب",
   run: async(client, message, args) => {
     if(!message.channel.name.startsWith("・〡الـطلبات")) return message.reply(`لا يمكنك الطلب هنا ، قم بالطلب في روم <#${config.order_room}>`).then(async (m) => {
-      await message.react("❌") 
-        setTimeout(() => m.delete(), 3000) 
+      await message.react("❌").catch(err => 0)
+        setTimeout(() => m.delete().catch(err => 0), 3000) 
   })
     let howmsg = new Discord.EmbedBuilder()
       .setTitle(`# Rolex Owner's .`)
@@ -17,9 +17,9 @@ module.exports = {
       طريقة الطلب : \`\`\`اكتب : #طلب ثم طلبك   \`\`\`**`)
       .setFooter({text: `Requested By : ${message.author.username} - # Rolex Owner's .`, iconURL: `https://cdn.discordapp.com/attachments/993590565767753838/1036697458060832828/rolexbot1.png`});
     const name1 = args.join(" ");
-    if(!name1) return message.reply({embeds: [howmsg]}).then(async(msggggggggg) => {
-      setTimeout(() => message.delete(), 3000)
-      setTimeout(() => msggggggggg.delete(), 3000);        
+    if(!name1) return message.reply({embeds: [howmsg]}).catch(err => 0).then(async(msggggggggg) => {
+      setTimeout(() => message.delete().catch(err => 0), 3000)
+      setTimeout(() => msggggggggg.delete().catch(err => 0), 3000);        
     })
     let ordernew = new Discord.EmbedBuilder()
       .setTitle(`# Rolex Owner's .`)
@@ -58,34 +58,34 @@ module.exports = {
       **الطلب :**
       \`\`\`${name1}\`\`\``)
       .setFooter({text: `Requested By : ${message.author.username} - # Rolex Owner's .`, iconURL: `${message.author.displayAvatarURL()}`});
-    message.reply({content: `ما هو نوع طلبك؟ ,<@${message.author.id}>`, embeds: [ordernew], components: [row]}).then(async (mmbtn) => {
+    message.reply({content: `ما هو نوع طلبك؟ ,<@${message.author.id}>`, embeds: [ordernew], components: [row]}).catch(err => 0).then(async (mmbtn) => {
       let collect = await mmbtn.awaitMessageComponent({ filter: b => b.user.id == message.author.id, max: 1 }).catch(() => 0);
       if(!collect) return;
       if(collect.customId.startsWith("cansel")) {
-        mmbtn.delete();
-        await message.reply({ content: `${message.author}, ❌ **تم إلغاء هذا الطلب بنجاح.**`, ephemeral: true }).then(async (msg1)=> {
-          setTimeout(() => message.delete(), 1000)
-          setTimeout(() => msg1.delete(), 3000);
+        mmbtn.delete().catch(err => 0);
+        await message.reply({ content: `${message.author}, ❌ **تم إلغاء هذا الطلب بنجاح.**`, ephemeral: true }).catch(err => 0).then(async (msg1)=> {
+          setTimeout(() => message.delete().catch(err => 0), 1000)
+          setTimeout(() => msg1.delete().catch(err => 0), 3000);
         })
         }
        if(collect.customId.startsWith("mntgat")) {
-          mmbtn.delete();
-          await message.reply({ content: `${message.author}, ✅ **تم إرسال هذا الطلب إلي البائعين بنجاح.**`, ephemeral: true }).then(async (msg2)=> {
+          mmbtn.delete().catch(err => 0);
+          await message.reply({ content: `${message.author}, ✅ **تم إرسال هذا الطلب إلي البائعين بنجاح.**`, ephemeral: true }).catch(err => 0).then(async (msg2)=> {
             let channel = message.guild.channels.cache.get(config.order_room_send);
             if(!channel) return;
             channel.send({ content: `** :طلب جديد من ${message.author} - <@&${config.order_mention_role}> ** <:RolexsPin:1012969637597675600>`, embeds: [ordernew1] })
-          setTimeout(() => message.delete(), 1000)
-          setTimeout(() => msg2.delete(), 3000);           
+          setTimeout(() => message.delete().catch(err => 0), 1000)
+          setTimeout(() => msg2.delete().catch(err => 0), 3000);           
         })
         }
        if(collect.customId.startsWith("tsamem")) {
           mmbtn.delete();
-          await message.reply({ content: `${message.author}, ✅ **تم إرسال هذا الطلب إلي البائعين بنجاح.**`, ephemeral: true }).then(async (msg3)=> {
+          await message.reply({ content: `${message.author}, ✅ **تم إرسال هذا الطلب إلي البائعين بنجاح.**`, ephemeral: true }).catch(err => 0).then(async (msg3)=> {
             let channel = message.guild.channels.cache.get(config.order_room_send);
             if(!channel) return;
-            channel.send({ content: `** :طلب جديد من ${message.author} - <@&${config.order_mention_role}> ** <:RolexsPin:1012969637597675600>`, embeds: [ordernew1] })
-          setTimeout(() => message.delete(), 1000)
-          setTimeout(() => msg3.delete(), 3000);            
+            channel.send({ content: `** :طلب جديد من ${message.author} - <@&${config.order_mention_role}> ** <:RolexsPin:1012969637597675600>`, embeds: [ordernew1] }).catch(err => 0)
+          setTimeout(() => message.delete().catch(err => 0), 1000)
+          setTimeout(() => msg3.delete().catch(err => 0), 3000);
         })
        }
     })
