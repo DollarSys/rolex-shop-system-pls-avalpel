5a1e084cc747ca83d25e6c159555e340e622df90
DollarSys <116037907+DollarSys@users.noreply.github.com>
2022-11-14 10:58:28 +0200

    Update messageCreate.js

--- events/order/messageCreate.js
+++ events/order/messageCreate.js
@@ -5,6 +5,6 @@ module.exports = {
   run: async(client, message) => {
     if(!message.guild) return;
     if(message.channel != config.order_room) return;
-    setTimeout(() => message.delete()..catch(err => 0), 10000);
+    setTimeout(() => message.delete().catch(err => 0), 10000);
     }
 }
